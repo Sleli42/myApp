6ab86d1ea3752ebf2ae28665a143fdda23e03ef9
Sleli42 <lubaujar@student.42.fr>
2015-11-12 14:53:56 +0100

    Thu 14:53

--- server.js
+++ server.js
@@ -2,25 +2,35 @@ var	express = require('express');
 var	morgan = require('morgan');
 var	port = process.env.PORT || 2121;
 var app = express();
-var server = require('http').createServer(app);
 //==== SOCKET.IO
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/uploads'));
 app.use(express.static(__dirname + '/views'));
 app.use(morgan('dev'));
 
-app.listen(port, function() {
+server.listen(port, function() {
 	console.log('Server running on port: ' + port);
 })
 
 app.get('/', function(req, res) {
 	res.render('index.ejs');
 });
-// client connect
+
+app.get('/signup', function(req, res) {
+	res.render('signup.ejs');
+})
+
+app.get('/login', function(req, res) {
+	res.render('login.ejs');
+})
+
+// SOCKET.IO - client connect
 io.on('connection', function(client) {
-	console.log('Client connected . . .');
+	//console.log('Client connected . . .');
 	client.on('join', function(data) {
 		console.log(data);
+		//client.emit('message', "Hello from server");
 	});
 });
