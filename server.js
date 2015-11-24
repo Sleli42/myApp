8d845a1871598ebb88b345ca847f5c4bf34b1fbc
Lucas BAUJARD <lubaujar@e2r7p14.42.fr>
2015-11-24 18:41:36 +0100

    Tue 18:42

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ var	express = require('express');
 var	morgan = require('morgan');
 var	port = process.env.PORT || 2121;
 var app = express();
-var mongoose = require('mongoose');
-var passport = require('passport');
-var cookieParser = require('cookie-parser');
-var flash = require('connect-flash');
+// var mongoose = require('mongoose');
+// var passport = require('passport');
+// var cookieParser = require('cookie-parser');
+// var flash = require('connect-flash');
 
-var session = require('express-session');
+// var session = require('express-session');
 //==== CONFIG DB
 //var configDB = require('./uploads/database.js');
 //mongoose.connect(configDB.url);
 
-mongoose.connect('mongodb://127.0.0.1:27017/db', function(err) {
-  if (err)
-  	console.log(err);
-});
+// mongoose.connect('mongodb://127.0.0.1:27017/db', function(err) {
+//   if (err)
+//   	console.log(err);
+// });
 
 /*
 //==== SOCKET.IO
@@ -26,18 +26,19 @@ app.use(express.static(__dirname + '/uploads'));
 app.use(express.static(__dirname + '/views'));
 
 app.use(morgan('dev'));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 //==== require for passport
-app.use(session({ secret: 'grosgrossecret',
-				resave: true, 
-				saveUninitialized: true })); //secret session
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+// app.use(session({ secret: 'grosgrossecret',
+// 				resave: true, 
+// 				saveUninitialized: true })); //secret session
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(flash());
 
 //==== routes
-require('./uploads/routes.js')(app, passport);
+// require('./uploads/routes.js')(app, passport);
+require('./uploads/routes.js')(app);
 
 app.listen(port, function() {
 	console.log('Server running on port: ' + port);
