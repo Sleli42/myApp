3cb512160b8b8a694c0f8b20e6e3b587fa4adac5
Lucas BAUJARD <lubaujar@e2r4p13.42.fr>
2016-10-24 18:07:24 +0200

    Mon 6:07PM

--- server.js
+++ server.js
@@ -2,54 +2,36 @@ var	express = require('express');
 var	morgan = require('morgan');
 var	port = process.env.PORT || 2121;
 var app = express();
-// var mongoose = require('mongoose');
-// var passport = require('passport');
-// var cookieParser = require('cookie-parser');
-// var flash = require('connect-flash');
 
-// var session = require('express-session');
-//==== CONFIG DB
-//var configDB = require('./uploads/database.js');
-//mongoose.connect(configDB.url);
+var multer = require('multer');
+var storage = multer.diskStorage(
+{
+	destination: function(req, file, callback) {
+		callback(null, './uploads/testApi');
+	},
+	filename: function(req, file, callback) {
+		callback(null, file.fieldname + '-' + Date.now());
+		// fieldname - Field name specified in the form
+	}
+});
+var upload = multer({ storage: storage }).single('fileName');
 
-// mongoose.connect('mongodb://127.0.0.1:27017/db', function(err) {
-//   if (err)
-//   	console.log(err);
-// });
-
-/*
-//==== SOCKET.IO
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-*/
 app.use(express.static(__dirname + '/uploads'));
 app.use(express.static(__dirname + '/views'));
-
 app.use(morgan('dev'));
-// app.use(cookieParser());
+require('./uploads/js/routes.js')(app);
 
-//==== require for passport
-// app.use(session({ secret: 'grosgrossecret',
-// 				resave: true, 
-// 				saveUninitialized: true })); //secret session
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(flash());
-
-//==== routes
-// require('./uploads/routes.js')(app, passport);
-require('./uploads/routes.js')(app);
+app.post('/api/photo', function(req, res) {
+	upload(req, res, function(err) {
+		if (err) {
+			console.log("Error uploading file.");å
+			// return res.end("Error uploading file.");
+		}
+		console.log("File is uploaded.");
+		// res.end("File is uploaded.")
+	})
+})
 
 app.listen(port, function() {
 	console.log('Server running on port: ' + port);
 })
-/*
-// SOCKET.IO - client connect
-io.on('connection', function(client) {
-	//console.log('Client connected . . .');
-	client.on('join', function(data) {
-		console.log(data);
-		//client.emit('message', "Hello from server");
-	});
-});
-*/
--- uploads/js/routes.js
+++ uploads/js/routes.js
@@ -15,23 +15,9 @@ module.exports = function(app, passport) {
 	app.get('/signup', function(req, res) {
 		res.render('signup.ejs');
 	});
-
-	//==== PROFIL SESSION
-	app.get('/profile', IsLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user: req.user
-		});
-	});
-
-	//==== LOG OUT
+	
 	app.get('logout', function(req, res) {
 		res.logout();
 		res.redirect('/');
 	});
 };
-
-function IsLoggedIn(req, res, next) {
-	if (req.isAuthenticated())
-		return next();
-	res.redirect('/');
-}
